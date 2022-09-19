334fcbe0ef3fc8eb42af53f695178fb2b676725c
YipChiSan <zye39@yahoo.com>
2022-09-19 20:44:01 +1000

    Add two virtual fields

--- models/User.js
+++ models/User.js
@@ -24,18 +24,22 @@ const userSchema = new Schema(
       type: Schema.Types.ObjectId,
       ref: 'Thought'
     }],
-    friends: {
+    friends: [{
       type: Schema.Types.ObjectId,
       ref: 'User'
-    },
+    }],
   },
   {
     toJSON: {
       getters: true,
+      virtuals: true,
     },
     id: false,
   }
 );
+userSchema.virtual("friendCount").get(function() {
+  return this.friends.length;
+});
 
 const User = model('user', userSchema);
 module.exports = User;
